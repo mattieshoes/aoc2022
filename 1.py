8d709556397a3e37beaa8540e4527f47683c98f1
Mattie Shoes <mattieshoes@gmail.com>
2022-12-02 09:14:15 -0700

    Simplified with nested list comprehensions

--- 1.py
+++ 1.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
 with open('1.txt') as f:
-    lines = f.readlines()
-lines = [line.rstrip('\n') for line in lines]
+    vals = sorted([sum([int(n) for n in line.split('\n')]) for line in f.read().split('\n\n')], reverse=True)
 
-temp_sum = 0
-calories = []
-for line in lines:
-    if not line:
-        calories.append(temp_sum)
-        temp_sum = 0
-    else:
-        temp_sum += int(line)
-calories.append(temp_sum)
-calories.sort(reverse=True)
-
-print(f"Max: {calories[0]}")
-print(f"Top Three: {sum(calories[:3])}")
+print(f"{vals[0]}\n{sum(vals[:3])}")
