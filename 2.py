3efbe5a7104bd2c7cafe25cc19f2918d74feebfb
Mattie Shoes <mattieshoes@gmail.com>
2022-12-02 08:18:44 -0700

    Cleaned up

--- 2.py
+++ 2.py
@@ -9,14 +9,10 @@ alt_score = 0
 for line in lines:
     letters = line.split(" ")
     vals = [ord(letters[0]) - ord('A'), ord(letters[1]) - ord('X')]
-    diff = (vals[1] - vals[0]) % 3
-    score += vals[1] + 1
-    if(diff != 2):
-        score += 3 + 3 * diff
-
-    alt_score += vals[1] * 3
-    diff = (vals[0] + vals[1] - 1) % 3
-    alt_score += diff + 1
+    result = (vals[1] - vals[0] + 1) % 3
+    score += vals[1] + 1 + result * 3
+    throw = (vals[0] + vals[1] - 1) % 3
+    alt_score += vals[1] * 3 + throw + 1
 
 print(f"Score: {score}")
 print(f"Alt score: {alt_score}")
