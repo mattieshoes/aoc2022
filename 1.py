85e16816b4378eeabea39aebc4189f9507b40089
Mattie Shoes <mattieshoes@gmail.com>
2022-12-03 12:24:55 -0700

    readability

--- 1.py
+++ 1.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
-with open('1.txt') as f:
-    vals = sorted(sum((int(n) for n in m.split('\n'))) for m in f.read().split('\n\n'))
 
-print(f"{vals[-1]}\n{sum(vals[-3:])}")
+#with open('1.txt') as f:
+#    elves = sorted(sum((int(n) for n in m.split('\n'))) for m in f.read().split('\n\n'))
+
+with open('1.txt') as f:
+    #split into elves based on double-newline
+    elves = f.read().split('\n\n')
+
+# Split each elf into items based on newline, so we have a list of lists
+elves = [elf.split('\n') for elf in elves]
+
+# Convert each item in each elf list into integers
+elves = [[int(n) for n in elf] for elf in elves]
+
+# replace the list of items with the sum of their values
+# so we now have a list of sums instead of a list of lists
+elves = [sum(elf) for elf in elves]
+
+# sort the list of sums
+elves.sort()
+
+print(f"{elves[-1]}\n{sum(elves[-3:])}")
