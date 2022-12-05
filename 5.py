d98f0e0fd5a257eadc8aa61f130ff19e44e31a7a
Mattie Shoes <mattieshoes@gmail.com>
2022-12-05 10:40:13 -0700

    Changed to rotating the layout for parsing

--- 5.py
+++ 5.py
@@ -2,28 +2,35 @@
 
 import re, copy
 
+# rotates a rectangular block of text
+def rotate(lines, clockwise=True):
+    t = ['' for i in range(len(lines[0]))]
+    if clockwise:
+        lines.reverse()
+    for i in range(len(lines[0])):
+        for line in lines:
+            t[i] += line[i]
+    return t
+
 with open('5.txt') as f:
     layout, lines = f.read().split('\n\n')
-    layout = layout.split('\n')
-    lines = lines.split('\n')
+layout = layout.split('\n')
+lines = lines.split('\n')
 
-loc = {}
 stacks = {}
 ordering = []
 
-# parse the last layout line for order, name, and location of stacks
-k = layout.pop()
-for pos in range(len(k)):
-    if k[pos] != ' ':
-        stacks[k[pos]] = []
-        loc[k[pos]] = pos
-        ordering.append(k[pos])
-
-# iterate backwards over the rest of the layout for stacks
-for line in reversed(layout):
-    for key in loc.keys():
-        if line[loc[key]] != ' ':
-            stacks[key].append(line[loc[key]])
+# rotate the layout clockwise so we can parse horizontally
+rotated = rotate(layout)
+for line in rotated:
+    name = line[0]
+    if name != ' ':
+        ordering += name
+        stacks[name] = []
+        for c in line[1:]:
+            if c == ' ':
+                break
+            stacks[line[0]] += c
 
 # make a deep copy of the stacks so part 1 and 2 can be independent
 stacks2 = copy.deepcopy(stacks)
@@ -34,12 +41,11 @@ for line in lines:
     f = vals[1]
     t = vals[2]
 
-    # for part 1, iteratively move one item at a time
-    for i in range(count):
-        item = stacks[f].pop()
-        stacks[t].append(item)
+    # for part 1, reverse the moved chunk
+    stacks[t] += reversed(stacks[f][-count:])
+    stacks[f] = stacks[f][:-count]
 
-    # for part 2, move all at once
+    # for part 2, no reverse
     stacks2[t] += stacks2[f][-count:]
     stacks2[f] = stacks2[f][:-count]
 
