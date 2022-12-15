0872af60d01677767042c0e6399c5a0d2051cb43
Mattie Shoes <mattieshoes@gmail.com>
2022-12-15 08:59:11 -0700

    Down to ~30 seconds runtime

--- 15.py
+++ 15.py
@@ -5,6 +5,14 @@ import re
 def distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
+def check(point):
+    if point[0] < 0 or point[0] > 4000000 or point[1] < 0 or point[1] > 4000000:
+        return False
+    for k in sensors.keys():
+        if distance(k, point) <= sensor_range[k]:
+            return False
+    return True
+
 ans1 = 0
 ans2 = 0
 sensors = {}
@@ -34,26 +42,34 @@ print(f"Part 1: {ans1}")
 for k in sensors.keys():
     sensor_range[k] = distance(k, sensors[k])
 
-found = False
-for x in range(4000000, -1, -1):
-    y = 0
-    while y < 4000001:
-        loc = (x, y)
-        max_ld = -999999
-        for key in sensors.keys():
-            ld = sensor_range[key] - distance(key, loc)
-            if ld > max_ld:
-                max_ld = ld
-        if max_ld >= 0:
-            y += max_ld + 1
-            continue
-        print(loc)
-        ans2 = loc[0] * 4000000 + loc[1]
-        found = True
-        break
-    if(found):
-        break
-            
 
-print(f"Part 2: {ans2}")
+seq = []
+for k in sensors.keys():
+    r = sensor_range[k]
+    p0 = (k[0] + r, k[1] + 1)
+    p1 = (k[0], k[1] - r - 1)
+    p2 = (k[0] - r - 1, k[1])
+    p3 = (k[0], k[1] + r + 1)
+    seq.append([p0, p1])
+    seq.append([p1, p2])
+    seq.append([p2, p3])
+    seq.append([p3, p0])
+
+found = False
+for s in seq:
+    xoff = 1
+    yoff = 1
+    if s[1][0] < s[0][0]:
+        xoff *= -1
+    if s[1][1] < s[0][1]:
+        yoff *= -1
+    for x, y in zip(range(s[0][0], s[1][0] + xoff, xoff), range(s[0][1], s[1][1]+yoff, yoff)):
+        if check((x, y)):
+            found = True
+            ans2 = x * 4000000 + y
+            print(f"Part 2: {ans2}")
+            break
+    if found:
+        break
+
 
